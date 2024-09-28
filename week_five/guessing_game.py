c5a0901849db3c8da0e0f9a017bd017827a9e285
ssnowbbear <noah.holderroberts@gmail.com>
2024-09-28 03:09:09 -0500

    guessing game that i got dragged through. my brain fried

--- week_five/guessing_game.py
+++ week_five/guessing_game.py
@@ -1,34 +1,68 @@
-# Guessing Game
+#---------------------------Guessing Game----------------------------
 from random import randint
-from random import choice
 
-# number of games played
-# number of guesses per game
-# average number of guesses
+play_again = ""
+guesses = [] 
+while play_again != 'no':
 
-while True:
     comp_choice = randint(1,100)
-    while True:
+
+    user_guess = ""
+    guess_count = 0
+    while user_guess != comp_choice:
+
         user_guess = int(input("Enter a guess between 1-100: "))
+        guess_count += 1
+
+        lie = randint(1,10) == randint(1,10)
+
         if user_guess > comp_choice:
-            print('Too high!')
+            if lie:
+                print('Too low!')
+            else:
+                print('Too high!')
+
         elif user_guess < comp_choice:
-            print('Too low!')
-        elif user_guess == comp_choice:
-            print('You got it!')
-            break
-            
-    play_again = input("Press 'n' to quit or any other key to continue: ")
-    play_again = play_again.strip().lower()
-    if (play_again == 'n'):
-        print("Thank you for playing!")
+            if lie:
+                print('Too high!')
+            else:
+                print('Too low!')
+
+    guesses.append(guess_count)
+    print('You got it!')
+    print(f'Number of Guesses: {guess_count}')
+        
+    play_again = input("Type 'No' to quit or anything else to continue: ").strip().lower()
+
+loop_count = 0 # times through the for loop
+for num in guesses:
+    if len(guesses) == 1:
+        print(f'Guesses per Game: {num}')
         break
+    if loop_count == (len(guesses))-1:
+        print(f'{num}')
+        break
+    if loop_count > 0 and loop_count < (len(guesses))-1:
+        print(f'{num}, ', end='')
+        loop_count += 1
+        continue
+    print(f'Guesses per Game: {num}, ', end='') # guesses per game
+    loop_count += 1
+
+print(f'Total Guesses: {sum(guesses)}') #total guesses
+print(f'Total Games: {len(guesses)}') #games count
+print(f'Average Number of Guesses: {int((sum(guesses))/(len(guesses)))}') #average of guesses
+
+print("\n\n\nThank you for playing!")        
+    
             
             
 
-# make the computer lie 10% of the time
-# when the guess is correct, computer always says correct
+# make the computer lie 10% of the time x
+# when the guess is correct, computer always says correct x
+# keep track of number of guesses required by user x
+# ask user if they want to play another game. x 
+# number of games played x 
+# number of guesses per game x
+# average number of guesses x
 
-# keep track of number of guesses required by user
-
-# ask user if they want to play another game. 
