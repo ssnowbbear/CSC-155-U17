5d2a7d30fff375833bff8b8c67b118f05d53d824
ssnowbbear <noah.holderroberts@gmail.com>
2024-10-18 21:45:16 -0500

    forgot to undo comments

--- week_seven/FIRST_MIDTERM.py
+++ week_seven/FIRST_MIDTERM.py
@@ -1,39 +1,39 @@
 # > 1 Program that counts vowels in a user inputted line.
 
-# user_line = str(input("Enter a line: ")).lower() #takes an input from the user and lower cases it
+user_line = str(input("Enter a line: ")).lower() #takes an input from the user and lower cases it
 
-# vowels = ["a", "e", "i", "o", "u"] #list of vowels
+vowels = ["a", "e", "i", "o", "u"] #list of vowels
 
-# vowel_count = 0 #keeps track of vowels in 
+vowel_count = 0 #keeps track of vowels in 
 
-# for letter in range(len(user_line)): #loops through sentence
-#     if user_line[letter] in vowels: #looks if letter in user_line is in list of vowels
-#         vowel_count += 1 #increases vowel count
+for letter in range(len(user_line)): #loops through sentence
+    if user_line[letter] in vowels: #looks if letter in user_line is in list of vowels
+        vowel_count += 1 #increases vowel count
 
-# print(f"Vowels used in your sentence: {vowel_count}")
+print(f"Vowels used in your sentence: {vowel_count}")
 #----------------------------------------------------------------------
 
 # > 2 How many people and their ages sorted oldest to youngest. 
     # Print the most common age
 
-# people = {}
+people = {}
 
-# people_count = (int(input("How many people? ").strip()))
+people_count = (int(input("How many people? ").strip()))
 
-# for int in (range(people_count)):
-#     name_of_people = input("Enter person and age: ").strip().capitalize().split()
-#     key, val = name_of_people
-#     people.update({key:val})
+for int in (range(people_count)):
+    name_of_people = input("Enter person and age: ").strip().capitalize().split()
+    key, val = name_of_people
+    people.update({key:val})
 
-# sort_people = sorted(people.items(), key=lambda x: x[1], reverse=True)
+sort_people = sorted(people.items(), key=lambda x: x[1], reverse=True)
 
-# print("\nOldest to youngest:")
-# for name, age in sort_people:
-#     print(f"{name}")
+print("\nOldest to youngest:")
+for name, age in sort_people:
+    print(f"{name}")
 
-# ages_list = list(people.values())
-# common_age = max(set(ages_list), key = ages_list.count)
-# print(f"\nMost common age: {common_age}")
+ages_list = list(people.values())
+common_age = max(set(ages_list), key = ages_list.count)
+print(f"\nMost common age: {common_age}")
 
 # import operator
 # sorted_name_age = sorted(name_age.items(), key=operator.itemgetter(1))
