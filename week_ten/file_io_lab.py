50a92d4a241ac33074aee571cd6236d72469f771
ssnowbbear <noah.holderroberts@gmail.com>
2024-11-13 23:11:03 -0600

    finished ? the lab, need to ask about problem 7

--- week_ten/file_io_lab.py
+++ week_ten/file_io_lab.py
@@ -27,13 +27,58 @@
 #     file.write('File 3 IO!')
 
 # > 1 - Total number of words in text file
-with open('week_ten/twocities_lab.txt', 'r', encoding = 'utf-8') as f:
-    word_count = 0
-    for sentence in f:
-        line = sentence.rstrip()
-        words = line.split()
-        # print(words) # make sure words were separated
-        word_count += len(words)
-print(word_count)
 
-# > 2 - Total number of distinct words in text file
+f = open("week_ten/twocities_lab.txt", "r")
+
+txtFile = f.read()
+twoCities = txtFile.split()
+word_count = len(twoCities)
+print(word_count) # prints number of words
+
+# 2 > Number of distinct words
+
+distinct_words =[]
+for word in twoCities:
+    if word not in distinct_words:
+        distinct_words.append(word)
+# print(distinct_words) # make sure it was separating by word
+# print(len(distinct_words))
+
+# > 3 The longest word and its length
+
+lengthiest_word = twoCities[0]
+for word in twoCities:
+    if len(word) > len(lengthiest_word):
+        lengthiest_word = word
+print(lengthiest_word) # the longest word as iterated above
+print(len(lengthiest_word)) # length of the longest word
+
+# > 4 Distinct words sorted by length
+
+# print(sorted(distinct_words, key = len, reverse = True)) # sorts longest to shortest
+# print(sorted(distinct_words, key = len)) # sorts shorteset to longest
+
+# > 5 Most common word
+
+word_counter = {}
+for word in twoCities:
+    if word in word_counter:
+        word_counter[word] += 1
+    else:
+        word_counter[word] = 1
+
+common_word = list(word_counter.keys())[0]
+for k, v in word_counter.items():
+    if v > word_counter[common_word]:
+        common_word = k
+
+print(common_word) # prints the most common word (the)
+
+# > 6 Words and their frequency sorted by frequency
+word_freq = {k: v for k,v in sorted(word_counter.items(), key = lambda item: item[1])} # can add 'reverse = True' for longest to shortest
+
+print(word_freq)
+
+# > 7 Punctuation ?
+
+f.close()
